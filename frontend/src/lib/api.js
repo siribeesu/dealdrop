fde4c83f97d88c1a2c7fd0710c21f03094ffa502
siribeesu <siribeesu07@gmail.com>
2026-02-22 00:16:14 +0530

    deploy

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_URL;
-
+console.log("API_BASE_URL:", API_BASE_URL); 
 // Helper function to get auth token
 const getAuthToken = () => {
   return localStorage.getItem('token');
